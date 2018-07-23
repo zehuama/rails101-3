245831e782d54e02999f10c1f195650103c4a66d
javiel <zehua046@gmail.com>
2018-07-23 21:37:28 +0800

    add validation to title

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -2,13 +2,18 @@ class GroupsController < ApplicationController
   def index
     @groups = Group.all
   end
+
   def new
     @group = Group.new
   end
+
   def create
     @group = Group.new(group_params)
-    @group.save
+    if @group.save
       redirect_to groups_path
+    else
+      render :new
+    end 
   end
 
   def show
